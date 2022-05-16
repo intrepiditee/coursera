a7ce3fbe158c6ecafd9a2a5a85d2126d50637df1
Rae Xu <raexu@google.com>
2022-05-16 16:43:54 -0700

    12345667

--- Algorithmic Thinking/Week1/Brute Force shortest path.py
+++ Algorithmic Thinking/Week1/Brute Force shortest path.py
@@ -41,6 +41,6 @@ def find_shortest_path(graph, source, target):
         for path in paths:
             if is_path(graph, path):
                 return path, length
-    return 1
+    return 123455667
 
 #print find_shortest_path({1:{2,3}, 2:{1, 5}, 3:{4}, 4:{5}, 5:{2}}, 1, 5)
